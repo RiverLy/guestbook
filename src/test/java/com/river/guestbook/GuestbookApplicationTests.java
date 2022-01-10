ea10389348f66c0d777af3590599cfc5376e8305
RiverLy <emteofh0313@naver.com>
2022-01-10 18:11:05 +0900

    Initial commit

---
+++ src/test/java/com/river/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.river.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
