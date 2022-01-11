6ac25581d1c811fb0891b1132edfb05d6d52c3ee
RiverLy <emteofh0313@naver.com>
2022-01-11 15:17:36 +0900

    [기능] 글 상세조회 기능 구현

--- src/main/java/com/river/guestbook/controller/GuestbookController.java
+++ src/main/java/com/river/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.river.guestbook.controller;
 
+import com.river.guestbook.dto.GuestbookDTO;
 import com.river.guestbook.dto.PageRequestDTO;
 import com.river.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -26,6 +30,26 @@ public class GuestbookController {
     public void list(PageRequestDTO pageRequestDTO, Model model){
         log.info("list..........");
         model.addAttribute("result", guestbookService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register......");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto...." + dto);
+        Long gno = guestbookService.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/read")
+    public void read(Long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model){
+        log.info("Gno : " + gno);
+        GuestbookDTO dto = guestbookService.read(gno);
+        model.addAttribute(dto);
     }
 
 }
